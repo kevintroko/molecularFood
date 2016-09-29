e48fbc5706c4e14459666860a0db26078caecde8
Kevin <kevintroko@gmail.com>
2016-09-29 16:11:28 -0500

    slider done

--- js/init.js
+++ js/init.js
@@ -4,5 +4,9 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+     $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
