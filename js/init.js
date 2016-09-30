1fe8039f2743895e47c377b5d561cd03d0949f13
Kevin <kevintroko@gmail.com>
2016-09-29 19:29:49 -0500

    nutricion

--- js/init.js
+++ js/init.js
@@ -8,5 +8,11 @@
       $('.slider').slider({full_width: true});
     });
 
+     $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+      
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
